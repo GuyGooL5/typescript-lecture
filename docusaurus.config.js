6625e1cc0e8fef40681fb2e1b9bf02b9194f697e
Guy Tsitsiashvili <guygool4@gmail.com>
2023-06-09 02:35:36 +0300

    feat: update project name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 const organizationName = "GuyGooL5";
-const projectName = "typescript-lectures";
+const projectName = "typescript-lecture";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
